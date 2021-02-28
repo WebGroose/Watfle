36d8c615505d2166d52df1d05353583a6cc17448
hoyoungishandsome <eric250@naver.com>
2021-02-28 20:35:16 +0900

    fix header location

--- html/server/write.php
+++ html/server/write.php
@@ -4,7 +4,7 @@ require_once 'config.php';
 session_start();
                
                 
- $id = $_SESSION['user_id'];                      //Writer
+$id = $_SESSION['user_id'];                      //Writer
 $title = $_GET['title'];                  //Title
 $content = $_GET['content'];              //Content
 $movie = $_GET['piece'];                //영화명
@@ -19,11 +19,11 @@ $query = 'insert into review (user_idx ,title, content, piece_id, rating) values
 $result = mysqli_query($connect, $query);
 if($result){
     $_SESSION['message'] = "리뷰가 등록되었습니다.";
-	header('location:piece.html?piece='.$movie);
+	header('location:../piece.html?piece='.$movie);
 }
 else{
     $_SESSION['message'] = "리뷰 등록에 실패하였습니다.";
-	header('location:piece.html?piece='.$movie);
+	header('location:../piece.html?piece='.$movie);
 }
  
 mysqli_close($connect);
